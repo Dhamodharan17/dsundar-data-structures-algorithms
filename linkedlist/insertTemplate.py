9f76ee97fb80b7238b69b38cb89dcfc76946dd27
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2025-10-24 23:00:08 +0530

    insert template for practice

---
+++ linkedlist/insertTemplate.py
@@ -0,0 +1,34 @@
+class DLLNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DLL:
+    head = None
+    
+    def insertAtStart(self, val):
+        return
+    def insertAtEnd(self, val):
+        return
+    def insertAtK(self,val):
+        return
+    def insertAfterNode(self,val):
+        return
+    def insertBeforeNode(self,val):
+        return
+    def insertInSortedDLL(self,val):
+        return
+    
+    def insertAtStartRec(self, val):
+        return
+    def insertAtEndRec(self, val):
+        return
+    def insertAtKRec(self,val):
+        return
+    def insertAfterNodeRec(self,val):
+        return
+    def insertBeforeNodeRec(self,val):
+        return
+    def insertInSortedDLLRec(self,val):
+        return
